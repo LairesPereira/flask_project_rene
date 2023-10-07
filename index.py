e6a7b9fcfe93994b49e69acedf76b8d56b4e98e8
Laires <lairespsoares@gmail.com>
2023-10-07 15:52:19 -0300

    pagina funcionando com cadastro de usuarios

--- index.py
+++ index.py
@@ -1,1 +1,47 @@
 from flask import *
+from datetime import *
+
+app = Flask(__name__)
+
+nome = 'Laires'
+
+usuarios = []
+
+@app.route('/receberNome', methods=['POST'])
+def receberNome():
+    nome = str(request.form.get('user'))
+    cargo = str(request.form.get('userCargo'))
+    senha = str(request.form.get('userPassword'))
+
+    usuarios.append(nome)
+
+    listao = ''
+
+    for user in usuarios:
+        listao = listao + '\n' + user
+
+    msg = 'senha nao pode ser igual ao nome'
+    if(senha == nome): 
+        return render_template('index.html', dev=nome, mensagem=msg)
+    
+    print(listao)
+    return render_template('sucesso.html', lista=listao)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html', dev=nome)
+
+
+@app.route('/hora')
+def horas():
+    data = datetime.now()
+    ano = data.year
+    mes = data.month
+    dia = data.day
+    hora = data.hour
+    minutos = data.minute
+    return f'Hoje, a data é {dia}-{mes}-{ano} e agora é {hora}:{minutos}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
